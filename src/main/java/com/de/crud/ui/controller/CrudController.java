cec70bfb8a30f6f46d35ca284cd5e5b347f5a87c
demirtaserdem <demirtaserdem@gmail.com>
2020-02-03 21:33:21 +0300

    Add Crud Controller and ServerPort Set

---
+++ src/main/java/com/de/crud/ui/controller/CrudController.java
@@ -0,0 +1,49 @@
+/**
+ * 
+ */
+package com.de.crud.ui.controller;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author demirtaserdem@gmail.com 3 Şub 2020
+ */
+
+@RestController
+@RequestMapping("/")
+public class CrudController {
+
+	@GetMapping()
+	public String getRequest() {
+		String getResponse;
+		getResponse = "Get Request Response";
+		return getResponse;
+	}
+
+	@PostMapping()
+	public String postRequest() {
+		String postResponse;
+		postResponse = "Post Request Response";
+		return postResponse;
+	}
+
+	@PutMapping()
+	public String putRequest() {
+		String putResponse;
+		putResponse = "Put Request Response";
+		return putResponse;
+	}
+
+	@DeleteMapping()
+	public String deleteRequest() {
+		String deleteResponse;
+		deleteResponse = "Delete Request Response";
+		return deleteResponse;
+	}
+}
